168ac02f3835ef79a6ef656088534653d2ad0706
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-09-17 12:08:24 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+//IIFE -- Immediately Invoked Function Expression
+(function()
+{
+
+    function Start()
+    {
+        console.log("Hello, World!");
+        console.log("This is the Repo Demo!");
+    }
+
+    window.addEventListener('load', Start);
+
+})();
